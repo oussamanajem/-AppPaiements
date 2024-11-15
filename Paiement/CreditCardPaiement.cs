ce3c406492f195f4e6b80ffffcd6fa7cf137e33e
Oussama Najem <300141420@monboreal.ca>
2024-11-14 23:31:35 -0500

    Initial commit with .gitignore

---
+++ Paiement/CreditCardPaiement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Paiement
+{
+    public class CreditCardPaiement : Paiement
+    {
+        // Property specific to CreditCardPaiement
+        public string CardNumber { get; set; }
+
+        // Constructor
+        public CreditCardPaiement(decimal amount, DateTime date, string cardNumber)
+        {
+            Amount = amount;
+            Date = date;
+            CardNumber = cardNumber;
+        }
+
+        // Override the abstract method DisplayDetails
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"Amount: {Amount}, Date: {Date.ToShortDateString()}, Card Number: {CardNumber}");
+        }
+    }
+}
---
+++ Paiement/Paiement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paiement
+
+{
+    public abstract class Paiement
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public abstract void DisplayDetails();
+    }
+}
